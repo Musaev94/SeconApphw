6e8d5095225c70c8dcf10f4bde037a3c7d410601
Мусаев94 <beksultan1923@gmail.com>
2021-12-23 22:17:39 +0600

    Initial Commit

---
+++ SeconApphw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeconApphw
+//
+//  Created by user on 23/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
